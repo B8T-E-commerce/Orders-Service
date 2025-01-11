49fc081cac830108dcf4314a9a7fca9ebda301b5
Jakub Jelinek <jelinej2012@gmail.com>
2025-01-11 17:17:40 +0100

    Code cleaned from commented code

--- Order-service/src/main/java/com/b8t/Order_service/business/OrderService.java
+++ Order-service/src/main/java/com/b8t/Order_service/business/OrderService.java
@@ -21,15 +21,6 @@ public class OrderService {
         this.orderMapper = orderMapper;
     }
 
-//    public OrderDomain createOrder(OrderDomain orderDomain) {
-//        // Create and populate OrderDAL entity
-//        OrderDAL orderDAL = orderMapper.convertDomainToOrderDAL(orderDomain);
-//        System.out.println("Created manually: " + orderDAL);
-//        System.out.println("Created manually (shipping addresses): " + orderDAL.getShippingAddresses());
-//        System.out.println("Created manually (ordered items): " + orderDAL.getOrderedItems());
-//        return orderMapper.convertOrderDALToDomain(orderRepository.save(orderDAL));
-//    }
-
     public OrderDomain createOrder(OrderDomain orderDomain) {
         // First validate input
         if (orderDomain == null) {
@@ -41,13 +32,6 @@ public class OrderService {
         if (orderDAL == null) {
             throw new IllegalStateException("Failed to map order domain to DAL");
         }
-
-        // Debug logging
-        System.out.println("Created manually: " + orderDAL);
-        System.out.println("Created manually (shipping addresses): " +
-                (orderDAL.getShippingAddresses() != null ? orderDAL.getShippingAddresses() : "null"));
-        System.out.println("Created manually (ordered items): " +
-                (orderDAL.getOrderedItems() != null ? orderDAL.getOrderedItems() : "[]"));
 
         // Save and convert back to domain
         return orderMapper.convertOrderDALToDomain(orderRepository.save(orderDAL));
